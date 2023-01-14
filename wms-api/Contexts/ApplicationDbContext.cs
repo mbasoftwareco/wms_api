6dadc2d94ff7bf4a1a69dcfe2dfbe7df453310f4
Agil3 Software <55604975+mbasoftwareco@users.noreply.github.com>
2023-01-14 12:01:37 -0500

    Create ApplicationDbContext.cs

---
+++ wms-api/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace wms_api.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+    }
+}
